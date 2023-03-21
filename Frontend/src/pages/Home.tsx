08b5a5b5e249c5768ad76d4a1667870067151462
Languzda <langusda@gmail.com>
2023-03-21 22:42:57 +0100

    .

--- Frontend/src/pages/Home.tsx
+++ Frontend/src/pages/Home.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+import "./styles/Home.scss";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <>
+      <header className="header">
+        <h1 className="header__title">Witaj w naszej Pizzerii</h1>
+        <p className="header__subtitle">
+          U nas dostaniesz najbardziej reaktywną pizze w mieście
+        </p>
+        <a href="/menu" className="header__button">
+          Zobacz menu{" "}
+        </a>
+      </header>
+    </>
+  );
 };
 
 export default HomePage;
